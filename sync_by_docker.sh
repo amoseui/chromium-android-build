55878d3a4fdeaea412b42e6a04afead28b45ac04
amoseui <amoseui@gmail.com>
2015-12-15 23:27:43 +0900

    change volume of gclient command
    Signed-off-by: amoseui <amoseui@gmail.com>

--- sync_by_docker.sh
+++ sync_by_docker.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 GCLIENT_SYNC="docker run -i -w /chromium -v `pwd`/chromium:/chromium -v `pwd`/depot_tools:/depot_tools -e PATH=$PATH:/depot_tools:/chromium/src/third_party/android_tools/sdk/build-tools/23.0.0/ amoseui/chromium-android-build-osx /bin/bash -c"
-$GCLIENT_SYNC "gclient runhooks"
-
-GCLIENT_SYNC="docker run -i -w /chromium/src -v `pwd`/chromium:/chromium -v `pwd`/depot_tools:/depot_tools -e PATH=$PATH:/depot_tools:/chromium/src/third_party/android_tools/sdk/build-tools/23.0.0/ amoseui/chromium-android-build-osx /bin/bash -c"
 $GCLIENT_SYNC "gclient sync"
+$GCLIENT_SYNC "gclient runhooks"
 
 # Remove containers
 docker rm `docker ps -a -q`
